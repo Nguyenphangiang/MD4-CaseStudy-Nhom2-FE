79eebf2ebafca987d26d7a177e6d553a336cfe92
Nguyenphangiang <phancruel@gmail.com>
2022-05-15 10:15:35 +0700

    fix front end

--- admin.js
+++ admin.js
@@ -1,7 +1,7 @@
-let data= localStorage.getItem("data");
-if (data == null) {
-    window.location.href = "../home/home.html"
-}
+// let data= localStorage.getItem("data");
+// if (data == null) {
+//     window.location.href = "../index.html"
+// }
 // } else {
 //     let obj = JSON.parse(data);
 //     let token = obj.accessToken;
@@ -17,7 +17,7 @@ if (data == null) {
     function deleteUser(id) {
         $.ajax({
             type: "DELETE",
-            url: `http://localhost:8080/sign-up/${id}`,
+            url: `http://localhost:8080/sign-in/${id}`,
             success: function () {
                 showListUser()
             }
@@ -31,7 +31,7 @@ if (data == null) {
                 'Content-Type': 'application/json'
             },
             type: "GET",
-            url: "http://localhost:8080/sign-up/list",
+            url: "http://localhost:8080/sign-in/list",
             success: function (data) {
                 let content = ''
                 for (let i = 0; i < data.length; i++) {
@@ -48,7 +48,43 @@ if (data == null) {
         })
     }
 showListUser();
+function login(){
+    event.preventDefault();
+    let name = $('#name').val();
+    let password = $('#password').val();
+    let user = {
+        "name": name,
+        "password":password
+    }
+    $.ajax({
+        headers: {
+            'Accept' : 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type:"POST",
+        url: "http://localhost:8080/auth/login",
+        data:JSON.stringify(user),
+        success:function (data){
+            localStorage.setItem("data",JSON.stringify(data));
+            let data1= localStorage.getItem("data");
+            let obj = JSON.parse(data1)
+            let name = obj.name;
+            // let token = obj.accessToken;
+            let role = obj.roles.pop().authority;
+            if(role === "ROLE_ADMIN"){
+                window.location.href = "index.html"
+                alert("Welcome to koibito " + name)
+            } else {
+                // window.location.href = "../home/home.html"
+                window.location.href = "index.html"
+                alert("Welcome to koibito " + name)
+            }
+        }
+    })
+}
+
 function logout(){
+    alert("Cảm ơn vì đã sử dụng dịch vụ");
     window.localStorage.clear();
-    location.href = '../home/home.html';
+    location.href = 'index.html';
 }
